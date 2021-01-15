fa65b10c0857528177de53b535266efbe0a6e321
Max Hoffman <max@dolthub.com>
2021-01-15 15:40:31 -0800

    Adds resume example

---
+++ dolt-demos/1-reproducibility-2-resume/succeeds_first.py
@@ -0,0 +1,35 @@
+import pickle
+
+from metaflow import FlowSpec, step, DoltDT, Parameter
+import pandas as pd
+from sklearn import tree
+
+class SucceedsFirstDemo(FlowSpec):
+
+    bar_version = Parameter('bar-version',  help="Specifc the tag for the input version", required=True)
+
+    @step
+    def start(self):
+        with DoltDT(run=self, doltdb_path='foo', branch=self.bar_version) as dolt:
+            self.df = dolt.read_table('bar')
+
+        self.next(self.middle)
+
+    @step
+    def middle(self):
+        with DoltDT(run=self, doltdb_path='foo', branch=self.bar_version) as dolt:
+
+            df = self.df
+            df["B"] = df["B"].map(lambda x: x*2)
+
+            dolt.write_table(table_name='baz', df=df, pks=['index'])
+
+        self.next(self.end)
+
+    @step
+    def end(self):
+        pass
+
+
+if __name__ == '__main__':
+    SucceedsFirstDemo()
---
+++ dolt-demos/1-reproducibility-2-resume/succeeds_second.py
@@ -0,0 +1,35 @@
+import pickle
+
+from metaflow import FlowSpec, step, DoltDT, Parameter, Flow
+import pandas as pd
+from sklearn import tree
+
+class SucceedsSecondDemo(FlowSpec):
+
+    bar_version = Parameter('bar-version',  help="Specifc the tag for the input version", required=True)
+
+    @step
+    def start(self):
+        with DoltDT(run=self, doltdb_path='foo', branch=self.bar_version) as dolt:
+            self.df = dolt.read_table('bar')
+        first = Flow("SucceedsFirstDemo").latest_successful_run
+
+        self.next(self.middle)
+
+    @step
+    def middle(self):
+        with DoltDT(run=self, doltdb_path='foo', branch=self.bar_version) as dolt:
+            df = self.df
+            df["B"] = df["B"].map(lambda x: x*2)
+
+            dolt.write_table(table_name='baz', df=df, pks=['index'])
+
+        self.next(self.end)
+
+    @step
+    def end(self):
+        pass
+
+
+if __name__ == '__main__':
+    SucceedsSecondDemo()
